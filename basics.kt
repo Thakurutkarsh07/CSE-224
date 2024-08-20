683e33982d19eed837a795d76787ff7ee3a0dba4
Utkarsh Pratap Singh <113666652+Thakurutkarsh07@users.noreply.github.com>
2024-08-20 15:09:27 +0530

    Array

--- basics.kt
+++ basics.kt
@@ -1,6 +1,11 @@
 fun main(){
-    val name = "\t Ram \n Ram \t Ram \n \t Ram "
-
-    print(name)
-
+    //Array of integers
+    val numbers = arrayOf(1,2,3,4,5,6)
+    println(numbers[0])
+    //Array o string
+    val words: Array<String> = arrayOf("utkarsh","Pratap","Singh")
+    println(words[0])
+    //Array of Multiples of 2
+    val multiple = Array(8) {i->i*2}
+    println(multiple[1])
 }
