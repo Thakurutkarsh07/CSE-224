0eefb6607896572839b28fe92e80434e353015a3
Utkarsh Pratap Singh <113666652+Thakurutkarsh07@users.noreply.github.com>
2024-08-21 11:49:17 +0530

    String Interpolation

--- basics.kt
+++ basics.kt
@@ -1,11 +1,7 @@
 fun main(){
-    //Array of integers
-    val numbers = arrayOf(1,2,3,4,5,6)
-    println(numbers[0])
-    //Array o string
-    val words: Array<String> = arrayOf("utkarsh","Pratap","Singh")
-    println(words[0])
-    //Array of Multiples of 2
-    val multiple = Array(8) {i->i*2}
-    println(multiple[1])
+    val name = "Utkarsh Pratap Singh"
+    println("Hello $name, How's your day going?")
+    val a = 10
+    val b = 20
+    println("The Sum of $a + $b = ${a+b}")
 }
