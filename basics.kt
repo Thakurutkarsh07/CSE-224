af7d2562a86fd27a11d432299950a2f85dd5b554
Utkarsh Pratap Singh <113666652+Thakurutkarsh07@users.noreply.github.com>
2024-08-21 13:45:00 +0530

    Array

--- basics.kt
+++ basics.kt
@@ -1,19 +1,43 @@
 fun main(){
-    greeting()
-    println(name())
-    println(greetName("Utkarsh"))//Function calling by passing arguments
-}
+    //Array of integers
+    val numbers = arrayOf(1,2,3,4,5,6)
+    println(numbers[0])
+    //Array o string
+    val words: Array<String> = arrayOf("utkarsh","Pratap","Singh")
+    println(words[0])
+    //Array of Multiples of 2
+    val multiple = Array(8) {i->i*2}
+    println(multiple[1])
+    //Accessing Elements
+    val firstNum = numbers[0]
+    val secondWord = words[1]
 
-fun greeting(){
-    println("Hello Utkarsh")
-}
-//Function returning value
-fun name(): String{
-    val name = "Utkarsh"
-    return name
-}
-//Function having parameter
-fun greetName(name: String): String{
-    val greet = "Hello $name"
-    return greet
+    println("First number in array: $firstNum")
+    println("Second word in array: $secondWord")
+
+    //Modifying Element of Array by index
+    numbers[0] = 10
+    multiple[0] = 1
+    println("First number in array: $firstNum")
+    println("First Multiple of 2 in array: ${multiple[0]}")
+
+    //Iterating over Array
+    //Using for loop
+    for (num in multiple) {
+        println(num)
+    }
+    //2-D Arrays
+    val arr = arrayOf(arrayOf(2,2,2,3,4,5),arrayOf(1,2,3,4,5,5),arrayOf(9,8,7,5,2,3))
+
+    val matrix: Array<Array<Int>> = arrayOf(arrayOf(2,2,2,3,4,5),arrayOf(1,2,3,4,5,5),arrayOf(1,2,3,75,72,75))
+
+    println(matrix[1][2])
+    println(arr[1][4])
+    //Print 2-D Arrays
+    for (i in arr) {
+        for(j in i) {
+            print("$j ")
+        }
+        println()
+    }
 }
